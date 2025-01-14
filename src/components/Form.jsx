bef32e0d99f9ee3a403ee129e1b603ed2ed7b66a
Emerson Díaz <emedzh@gmail.com>
2025-01-14 11:59:41 -0600

    feat(Form): created fillInputs function
    - Created prevDefault function to prevent default actions of the form
    - Created fillInput function to fill an useState object with inputs value

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 function Form() {
+    const [input, setInput] = useState({
+        name: "",
+        description: ""
+    }) 
+    function prevDefault(e)  {
+        e.preventDefault();
+    }
+
+    function fillInput(e) {
+        const {name, value} = e.target;
+        setInput({...input, [name]: value})
+    }
     return (
-        <form>
+        <form onSubmit={prevDefault}>
             <div>
                 <label>Task name:</label>
-                <input type="text"/>
+                <input type="text" name="name" value={input.name} onChange={fillInput}/>
             </div>
             
             <div>
                 <label>Task description:</label>
-                <input type="text"/>
+                <input type="text" name="description" value={input.description} onChange={fillInput}/>
             </div>
 
             <input type="submit" value="Create Task"/>
