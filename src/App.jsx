4ac8dc7e82fb0ac321466a61aedf127027153dda
Emerson Díaz <emedzh@gmail.com>
2025-01-14 11:35:09 -0600

    Feat: Created Todo component
    - Created HTML structure of component Todo to contains all tasks
    - Component imported into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "normalize.css"
 import Form from "./components/Form"
+import Todo from "./components/Todo"
 
 function App() {
   return (
     <>
       <Form/>
+      <Todo/>
     </>
   )
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+function Todo() {
+    return (
+        <main>
+            <h1>Tasks</h1>
+
+            <div>
+                
+            </div>
+        </main>
+    )
+}
+
+export default Todo
