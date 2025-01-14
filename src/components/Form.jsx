323164bb33110008dc4eb2442c70e50cc3280c09
Emerson Díaz <emedzh@gmail.com>
2025-01-14 14:05:34 -0600

    feat(Form): add validateForm function
    - created validateForm function to check for empty inputs
    - display an alert when inputs are empty
    - validateForm called within the preventDefault function to ensure validation before form submission.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,32 @@
 import { useState } from "react";
 
 function Form() {
+    const [alert, setAlert] = useState("");
     const [input, setInput] = useState({
         name: "",
         description: ""
     }) 
+
     function prevDefault(e)  {
         e.preventDefault();
+        validateForm();
     }
 
     function fillInput(e) {
         const {name, value} = e.target;
         setInput({...input, [name]: value})
+    }
+    
+    function validateForm() {
+        setTimeout(() => {
+            setAlert("");
+        }, 3000);
+        if(Object.values(input).some(input => input === "")) {
+            setAlert("All fields are required");
+            return;
+        }
+
+        setAlert("Task added");
     }
     return (
         <form onSubmit={prevDefault}>
@@ -26,6 +41,8 @@ function Form() {
             </div>
 
             <input type="submit" value="Create Task"/>
+
+            <p>{alert}</p>
         </form>
     )
 }
